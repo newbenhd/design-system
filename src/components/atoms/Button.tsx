4908c66882d19bc994a8c6b9e91348974375191b
Benjamin Morrison <benjamin@Youngs-MacBook-Pro.local>
2022-01-31 13:20:18 -0800

    setup snapshot testing with storybook

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+interface IProps {
+    name: string;
+    size?: 'small' | 'medium' | 'larg';
+    type?: 'primary' | 'stanard' | 'error';
+}
+export default (props: IProps) => <button>
+    
+</button>
